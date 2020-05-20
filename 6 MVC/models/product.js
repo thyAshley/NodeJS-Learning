5785744a39841fd6e4ef9847a255db0f7551a159
Ashley <thy.ashley@gmail.com>
2020-05-20 18:20:36 +0800

    re-factor code for product.js

--- 6 MVC/models/product.js
+++ 6 MVC/models/product.js
@@ -1,35 +1,32 @@
 const fs = require('fs');
 const path = require('path');
+const rootDir = path.dirname(process.mainModule.filename);
+const p = path.join(rootDir, '/data', 'products.json');
 
+const getProductsFromFile = (cb) => { 
+    fs.readFile(p, (err, content) => {
+        if (err) {
+            return cb([]);
+        } else {
+            return cb(JSON.parse(content));
+        }
+    })
+}
 module.exports = class Product {
     constructor(title) {
         this.title = title;
     }
 
     save() {
-        const rootDir = path.dirname(process.mainModule.filename);
-        const p = path.join(rootDir, '/data', 'products.json');
-        fs.readFile(p, (err, content) => {
-            let products = [];
-            if (!err) {
-                products = JSON.parse(content);
-            } 
+        getProductsFromFile((products) => {
             products.push(this);
             fs.writeFile(p, JSON.stringify(products), (err) => {
                 console.log(err);
             });
-        })
+        });
     }
 
     static fetchAll(cb) {
-        const rootDir = path.dirname(process.mainModule.filename);
-        const p = path.join(rootDir, '/data', 'products.json');
-        fs.readFile(p, (err, content) => {
-            if (err) {
-                cb([]);
-            } else {
-                cb(JSON.parse(content));
-            }
-        })
+        getProductsFromFile(cb);
     }
 }
