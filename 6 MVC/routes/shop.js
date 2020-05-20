0f9e5816b03bdc915d4c9ad5b772cc0f06e5b3ca
Ashley <thy.ashley@gmail.com>
2020-05-20 16:15:55 +0800

    introduced MVC and refactor all the functionality that involve products searching to a productController

---
+++ 6 MVC/routes/shop.js
@@ -0,0 +1,9 @@
+const express = require('express');
+
+const productController = require('../controllers/products');
+
+const router = express.Router();
+
+router.get('/', productController.getProduct);
+
+module.exports = router;
