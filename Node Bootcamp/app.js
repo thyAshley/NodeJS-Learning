0f07aceaf05de9cbdac7af5f9629010720416b14
Ashley <thy.ashley@gmail.com>
2020-05-24 21:56:18 +0800

    added api to find specific tour by id

--- Node Bootcamp/app.js
+++ Node Bootcamp/app.js
@@ -20,9 +20,27 @@ app.get('/api/v1/tours', (req, res) => {
     });
 })
 
+app.get('/api/v1/tours/:id?', (req, res) => {
+    const tourId = +req.params.id;
+    const tour = tours.find((tour) => {
+        return tour.id === tourId;
+    })
+    if (tour) {
+        res.status(200).json({
+            status: 'success',
+            tour: tour
+        })
+    } else {
+        res.status(404).json({
+            status: 'fail',
+            message: 'Invalid ID'
+        })
+    }
+})
+
 app.post('/api/v1/tours', (req, res) => {
-    const newID = tours.length;
-    const newTour = {id: newID, ...req.body};
+    const postID = tours.length;
+    const newTour = {id: postID, ...req.body};
     tours.push(newTour);
     fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
         res.status(201).json({
@@ -34,6 +52,9 @@ app.post('/api/v1/tours', (req, res) => {
     })
 })
 
+app.patch('/api/v1/tours/:id', (req, res) => {
+    const postID = 
+})
 app.listen(port, () => {
     console.log(`server started at on port ${port}`);
 })
