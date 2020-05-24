629b696f88ea2721c7271dc69e5793eed5b50330
Ashley <thy.ashley@gmail.com>
2020-05-24 12:32:26 +0800

    convert file into promises with resolve and reject

--- Node Bootcamp/index.js
+++ Node Bootcamp/index.js
@@ -1,15 +1,39 @@
 const fs = require('fs');
 const superagent = require('superagent');
 
-fs.readFile(`${__dirname}/dog.txt`, (err, data) => {
-    console.log(`Breed: ${data}`);
-    
-    superagent.get(`https://dog.ceo/api/breed/${data}/images/random/3/2/1/`)
-    .end((err, res) => {
-        if (err) return console.log(err.message);
-        fs.writeFile('dog-img.txt', res.body.message, (err) => {
-            if (err) return console.log(err);
-            console.log('image saved to file');
+const readFile = (file) => {
+    return new Promise((resolve, reject) => {
+        fs.readFile(file, (err, data) => {
+            if (err) {
+                reject('file cannot be found');
+            } else {
+                resolve(data);
+            }
+        });
+    });
+}
+
+const writeFile = (file, data) => {
+    return new Promise((resolve, reject) => {
+        fs.writeFile(file, data, (err) => {
+            if (err) reject(err);
+            resolve('Image saved to file');
         })
     })
+}
+readFile(`${__dirname}/dog.txt`).then((data) => {
+    superagent.get(`https://dog.ceo/api/breed/${data}/images/random`).then(res => {
+        writeFile(`${__dirname}/dog-img.txt`, data).then(res => {
+            console.log(res);
+        })
+        .catch(err => {
+            console.log(err);
+        });
+    })
+    .catch(err => {
+        console.log(err.message);
+    })
+})
+.catch((err) => {
+    console.log(err);
 })
