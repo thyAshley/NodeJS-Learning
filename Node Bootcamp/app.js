fd279c418c4c9e8eb95855f05b0cfe9cd32f697f
Ashley <thy.ashley@gmail.com>
2020-05-24 19:37:12 +0800

    added post api to add new tours

--- Node Bootcamp/app.js
+++ Node Bootcamp/app.js
@@ -6,12 +6,33 @@ const port = 3000;
 
 const fs = require('fs');
 
-const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours.json`));
+const tours = JSON.parse(fs.readFileSync(`${__dirname}/dev-data/data/tours-simple.json`));
+
+app.use(express.json());
 
 app.get('/api/v1/tours', (req, res) => {
-    res.status(200).json(tours);
+    res.status(200).json({
+        status: 200,
+        results: tours.length,
+        data: {
+            tours
+        }
+    });
 })
 
+app.post('/api/v1/tours', (req, res) => {
+    const newID = tours.length;
+    const newTour = {id: newID, ...req.body};
+    tours.push(newTour);
+    fs.writeFile(`${__dirname}/dev-data/data/tours-simple.json`, JSON.stringify(tours), (err) => {
+        res.status(201).json({
+            status: 'success',
+            data: {
+                tour: newTour
+            }
+        })
+    })
+})
 
 app.listen(port, () => {
     console.log(`server started at on port ${port}`);
