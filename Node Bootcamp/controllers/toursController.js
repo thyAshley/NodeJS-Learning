d7653e07b6401043cd06b62a8d0cc9a7869d5769
Ashley <thy.ashley@gmail.com>
2020-05-26 11:49:03 +0800

    pagination

--- Node Bootcamp/controllers/toursController.js
+++ Node Bootcamp/controllers/toursController.js
@@ -37,8 +37,21 @@ exports.getAllTours = async (req, res) => {
             query = query.select('-__v');
         }
 
-        // execute query
+        // 5) Pagination
+        const page = +req.query.page || 1;
+        const limit = +req.query.limit || 5;
+        const skip = (page - 1) * limit
+        query = query.skip(skip).limit(limit);
 
+        if (req.query.page) {
+            const numTours = await Tour.countDocuments();
+
+            if (skip >= numTours) {
+                throw new Error('failure');
+            }
+        }
+
+        // execute query
         const tours = await query;
 
         // send response
