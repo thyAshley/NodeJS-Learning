5802dced6e4faca95a2a39d4ebc49241d94c306b
Ashley <thy.ashley@gmail.com>
2020-05-27 10:36:34 +0800

    added secret tour and query middleware to filter out

--- Node Bootcamp/models/tourModel.js
+++ Node Bootcamp/models/tourModel.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-
+const slugify = require('slugify');
 const tourSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -53,8 +53,46 @@ const tourSchema = new mongoose.Schema({
         default: Date.now(),
         select: false
     },
-    startDates: [Date]
+    slug: String,
+    startDates: [Date],
+    secretTour: {
+        type: Boolean,
+        default: false
+    }
+}, {
+    toJSON: { virtuals: true },
+    toObject: { virtuals: true }
 });
+
+tourSchema.virtual('durationInWeeks')
+.get(function() {
+    return this.duration / 7
+});
+
+// Query Middleware
+tourSchema.pre(/^find/, function(next) { 
+    this.find({secretTour: { $ne: true }});
+    this.select(['-secretTour']);
+    this.startQuery = Date.now();
+    next();
+});
+
+tourSchema.post(/^find/, function(doc, next) {
+    console.log(Date.now() - this.startQuery, 'milliseconds');
+    next();
+})
+// Document Middleware
+// tourSchema.pre('save', function(next) {
+//     this.slug = slugify(this.name, { lower: true });
+//     next();
+// });
+
+
+// tourSchema.post('save', function(doc, next){
+//     console.log(doc);
+//     next();
+// });
+ 
 
 const Tour = mongoose.model('Tour', schema=tourSchema);
 
