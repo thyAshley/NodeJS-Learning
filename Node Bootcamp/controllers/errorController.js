4ba9ace84bad38c885e0666427e60b36968dbc79
Ashley <thy.ashley@gmail.com>
2020-05-28 09:22:04 +0800

    added validationError

--- Node Bootcamp/controllers/errorController.js
+++ Node Bootcamp/controllers/errorController.js
@@ -11,6 +11,12 @@ const handleDuplicateFieldsDB = (err) => {
     return new AppError(message,400);
 }
 
+const handleValidationErrorDB = (err) => {
+    const errors = Object.values(err.errors).map(el => el.message);
+    const message = `Invalid input data. ${errors.join('. ')}`;
+    return new AppError(message, 400);
+}
+
 const sendErrorDev = (err, res) => {    
     res.status(err.statusCode).json({
         status: err.status,
@@ -47,6 +53,7 @@ exports.getError = (err, req, res, next) => {
         let error = {...err};
         if (error.name === 'CastError') error = handleCastErrorDB(error)
         if (error.code === 11000) error = handleDuplicateFieldsDB(error)
+        if (error.name === 'ValidationError') error = handleValidationErrorDB(error);
         sendErrorProd(error, res);
     }
 }
