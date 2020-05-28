3e144fac2cb7a8e37951e44ff39cc3df4d3b33a5
Ashley <thy.ashley@gmail.com>
2020-05-28 11:51:49 +0800

    added json web tokens which is issued after user sign up

--- Node Bootcamp/controllers/authenticationController.js
+++ Node Bootcamp/controllers/authenticationController.js
@@ -1,11 +1,23 @@
 const User = require('../models/userModel');
+
 const catchAsync = require('../utils/catchAsync')
 
-exports.signup = catchAsync(async (req, res, next) => {
-    const newUser = await User.create(req.body);
+const jwt = require('jsonwebtoken');
 
+exports.signup = catchAsync(async (req, res, next) => {
+    const newUser = await User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: req.body.password,
+        passwordConfirm: req.body.passwordConfirm
+    });
+
+    const token = jwt.sign({ id: newUser._id }, process.env.JWT_SECRET, {
+        expiresIn: process.env.JWT_EXPIRES_IN
+    });
     res.status(201).json({
         status: 'success',
+        token: token,
         data: {
             user: newUser
         }
