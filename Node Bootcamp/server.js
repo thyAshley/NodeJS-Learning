6438f552401c5b0d43155addc852edc687dd46e5
Ashley <thy.ashley@gmail.com>
2020-05-28 09:28:52 +0800

    added unhandled rejection and closing of server

--- Node Bootcamp/server.js
+++ Node Bootcamp/server.js
@@ -19,6 +19,14 @@ mongoose.connect(DB,
 
 
 // -- Start Server --
-app.listen(port, () => {
+const server = app.listen(port, () => {
     console.log(`server started at on port ${port}`);
+})
+
+process.on('unhandledRejection', err => {
+    console.log(err.name, err.message);
+    console.log('unhandled reject!, shutting down application');
+    server.close(() => {
+        process.exit(1);
+    });
 })
