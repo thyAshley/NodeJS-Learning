4add33428fe56bad7200b9f667a888868fae6b55
Ashley <thy.ashley@gmail.com>
2020-05-31 10:30:13 +0800

    change embedded guide for tours to referenced type data

--- Node Bootcamp/models/tourModel.js
+++ Node Bootcamp/models/tourModel.js
@@ -112,7 +112,12 @@ const tourSchema = new mongoose.Schema({
             day: Number
         }
     ],
-    guides: Array
+    guides: [
+        { 
+            type: mongoose.Schema.ObjectId,
+            ref: 'User'
+        }
+    ]
 }, {
     toJSON: { virtuals: true },
     toObject: { virtuals: true }
@@ -148,13 +153,12 @@ tourSchema.pre('save', function(next) {
     next();
 });
 
-tourSchema.pre('save', async function(next) {
-    const guidesPromises = this.guides.map(async id => {
-        return await User.findById(id);
-    })
-    this.guides = await Promise.all(guidesPromises);
-
-})
+// tourSchema.pre('save', async function(next) {
+//     const guidesPromises = this.guides.map(async id => {
+//         return await User.findById(id);
+//     })
+//     this.guides = await Promise.all(guidesPromises);
+// })
 
 const Tour = mongoose.model('Tour', schema=tourSchema);
 
