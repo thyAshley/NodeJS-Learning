fbcf466851d5e6423f49522ed4066d3e32be0259
Ashley <thy.ashley@gmail.com>
2020-06-02 10:20:47 +0800

    added overview pug which render website dynamically

--- Node Bootcamp/controllers/viewsController.js
+++ Node Bootcamp/controllers/viewsController.js
@@ -1,11 +1,21 @@
-exports.getOverview = (req, res) => {
+const Tour = require('../models/tourModel');
+const catchAsync = require('../utils/catchAsync');
+
+exports.getOverview = catchAsync( async (req, res, next) => {
+    // 1) Get Tour data from collection
+    const tours = await Tour.find();
+
+    // 2) Build Template
+    
+    // 3) Render template using tour data
     res.status(200).render('overview.pug', {
-        title: 'All Tours'
+        title: 'All Tours',
+        tours
     });
-}
+})
 
 exports.getTour = (req, res) => {
-        res.status(200).render('tour.pug', {
-            title: 'The Forest Hiker'
-        })
+    res.status(200).render('tour.pug', {
+        title: 'The Forest Hiker'
+    })
 }
