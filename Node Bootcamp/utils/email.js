8da62177d41c35acfafc32bc081a7beebc52cb0c
Ashley <thy.ashley@gmail.com>
2020-06-04 14:56:35 +0800

    added Email class

--- Node Bootcamp/utils/email.js
+++ Node Bootcamp/utils/email.js
@@ -1,27 +1,40 @@
 const nodemailer = require('nodemailer');
 
-const sendEmail = async (options) => {
-    // 1) Transporter
-    const transporter = nodemailer.createTransport({
-        host: process.env.EMAIL_HOST,
-        port: process.env.EMAIL_PORT,
-        auth: {
-          user: process.env.EMAIL_USERNAME,
-          pass: process.env.EMAIL_PASSWORD
-        }
-      });
-    // 2 Define the email options
-    const mailOption = {
-        from: 'Dev-Team <dev@natours.io>',
-        to: options.email,
-        subject: options.subject,
-        text: options.message,
-        //html: 
+module.exports = class Email {
+  constructor(user, url) {
+    this.to = user.email;
+    this.firstName = user.name.split(' ')[0];
+    this.url = url;
+    this.from = `Dev-Team <${process.env.EMAIL_FROM}>`;
+  }
+
+  createTransport() {
+    if (process.env.MODE === 'production') {
+      // Sendgrid
+      return 1
+    } else {
+        const transporter = nodemailer.createTransport({
+          host: process.env.EMAIL_HOST,
+          port: process.env.EMAIL_PORT,
+          auth: {
+            user: process.env.EMAIL_USERNAME,
+            pass: process.env.EMAIL_PASSWORD
+          }
+        });
     }
+  }
 
-    // 3) Send email with nodemailer
+  send(template, subject) {
+    const mailOption = {
+      from: 'Dev-Team <dev@natours.io>',
+      to: options.email,
+      subject: subject,
+      text: options.message
+    }
+      await transporter.sendMail(mailOption);
+  }
 
-    await transporter.sendMail(mailOption);
+  sendWelcome() {
+    this.send('Welcome', 'Welcome to the Natours Family!')
+  }
 }
-
-module.exports = sendEmail;
